58fdf70a1f52db16dde17323a082c3d06d7270bb
Mthobeli Makaluza <mthobelimakaluza@gmail.com>
2026-07-14 15:23:10 +0200

    root and health endpoints

--- first_crud_api.js
+++ first_crud_api.js
@@ -3,7 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(
+    { "name": "Task API", 
+      "version": "1.0" , 
+      "endpoints": ["/tasks"]
+    });
+});
+
+app.get('/health', (req, res) => {
+  res.send(
+    { "status": "OK" }
+  );
 });
 
 app.listen(port, () => {
